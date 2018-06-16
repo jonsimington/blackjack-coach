e0a5e268d4427564735444baf6d07d87889319de
Jon Simington <jon@simington.io>
2018-06-15 23:01:08 -0500

    Initial xcode project commit

---
+++ Blackjack Coach/Blackjack Coach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Blackjack Coach
+//
+//  Created by Jon Simington on 6/15/18.
+//  Copyright © 2018 Jon Simington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
