585c3b6139a2a1656e077000e4f10761e41dd483
Jon Simington <jon@simington.io>
2018-06-16 01:29:47 -0500

    Add base UI elements

--- Blackjack Coach/Blackjack Coach/ViewController.swift
+++ Blackjack Coach/Blackjack Coach/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // DEALER VIEWS
+    @IBOutlet var dealerHandContainer: UIView!
+    @IBOutlet var dealerCard1: UIImageView!
+    @IBOutlet var dealerCard2: UIImageView!
+
+    // PLAYER VIEWS
+    @IBOutlet var playerHandContainer: UIView!
+    @IBOutlet var playerCard1: UIImageView!
+    @IBOutlet var playerCard2: UIImageView!
+
+    @IBOutlet var playerControlsContainer: UIView!
+    @IBOutlet var playerHitButton: UIButton!
+    @IBOutlet var playerStandButton: UIButton!
+    @IBOutlet var playerSplitButton: UIButton!
+    @IBOutlet var playerDoubleDownButton: UIButton!
+    @IBOutlet var playerInsuranceButton: UIButton!
+    @IBOutlet var playerSurrenderButton: UIButton!
+
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
